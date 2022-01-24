9dcb0c546b302a31633204e45019d18ad30f8b08
qujianping <qujianping@bytedance.com>
2022-01-24 14:20:01 +0800

    zk
    Change-Id: I98dabab1c821039d58c717ba97596781996517df

--- zk/start.sh
+++ zk/start.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-docker run --name myzk --rm -p 12181:2181 --name myzk  -d zookeeper
+PORT=$1
+if [ -z "$PORT" ];then
+	echo "need port"
+	exit 1
+fi
+docker run --name zookeeper-$PORT --rm -p $PORT:2181  -d zookeeper
+
+STOPFILE=stop-$PORT.sh
+(cat<<EOF
+#!/bin/bash
+docker stop zookeeper-$PORT
+rm -f \$0
+EOF
+) > $STOPFILE
+chmod +x $STOPFILE
